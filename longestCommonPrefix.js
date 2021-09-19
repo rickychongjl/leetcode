ce7a036c7e46c1e4371b5c66f28f1e5ee6cbfe17
rickyChong(person pc) <jlchong2008@gmail.com>
2021-09-19 16:26:41 +1000

    added optimised mem usage for longest prefix

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -1,3 +1,22 @@
+// /**
+//  * @param {string[]} strs
+//  * @return {string}
+//  */
+//  var longestCommonPrefix = function(strs) {
+//     if(strs.length <= 0)
+//         return "";
+    
+//     var prefix = "";
+//     for(var i=0; i<strs[0].length; i++){
+//         for(var j=1; j<strs.length; j++){
+//             if(strs[j][i] !== strs[0][i]){
+//                 return strs[0].slice(0, i);
+//             }
+//         }
+//     }
+//     return strs[0];
+// };
+//Optimised mem usuage
 // https://leetcode.com/problems/longest-common-prefix/submissions/
 /**
  * @param {string[]} strs
@@ -6,15 +25,14 @@
 var longestCommonPrefix = function(strs) {
     if(strs.length <= 0)
         return "";
-    
-    var prefix = "";
-    for(var i=0; i<strs[0].length; i++){
-        for(var j=1; j<strs.length; j++){
-            if(strs[j][i] !== strs[0][i]){
-                return strs[0].slice(0, i);
-            }
+
+    var prefix = strs[0];
+    var xx = strs.reduce((previousValue, currentValue) => {
+        var i = 0;
+        while(i<previousValue.length && i<currentValue.length && previousValue[i] === currentValue[i]){
+            i++;
         }
-    }
-    return strs[0];
+        return previousValue.slice(0, i);
+    }, prefix);
+    return xx;
 };
-longestCommonPrefix(["flower","flow","floght"])
